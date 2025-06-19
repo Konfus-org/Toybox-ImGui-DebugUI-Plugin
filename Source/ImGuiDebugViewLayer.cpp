dc1f18fe4477426742af7669048768472a2da30b
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-06-19 16:01:21 -0700

    Update ImGuiDebugViewLayer.cpp

--- Source/ImGuiDebugViewLayer.cpp
+++ Source/ImGuiDebugViewLayer.cpp
@@ -66,89 +66,89 @@ namespace ImGuiDebugView
         const ImGuiIO& io = ImGui::GetIO();
 
         // Start the Dear ImGui frame
-        {
-            ImGui::NewFrame();
-        }
+        //{
+        //    ImGui::NewFrame();
+        //}
 
-        // Listen for key press to toggle the debug window
-        {
-            if (Tbx::Input::IsKeyDown(TBX_KEY_F1))
-            {
-                _showDebugWindowOnDebugBtnUp = true;
-            }
-            if (_showDebugWindowOnDebugBtnUp &&
-                Tbx::Input::IsKeyUp(TBX_KEY_F1))
-            {
-                _showDebugWindowOnDebugBtnUp = false;
-                _isDebugWindowOpen = !_isDebugWindowOpen;
-            }
-        }
+        //// Listen for key press to toggle the debug window
+        //{
+        //    if (Tbx::Input::IsKeyDown(TBX_KEY_F1))
+        //    {
+        //        _showDebugWindowOnDebugBtnUp = true;
+        //    }
+        //    if (_showDebugWindowOnDebugBtnUp &&
+        //        Tbx::Input::IsKeyUp(TBX_KEY_F1))
+        //    {
+        //        _showDebugWindowOnDebugBtnUp = false;
+        //        _isDebugWindowOpen = !_isDebugWindowOpen;
+        //    }
+        //}
 
-        // Show debug window
-        if (_isDebugWindowOpen)
-        {
-            ImGui::Begin("Tbx Debugger", &_isDebugWindowOpen);
+        //// Show debug window
+        //if (_isDebugWindowOpen)
+        //{
+        //    ImGui::Begin("Tbx Debugger", &_isDebugWindowOpen);
 
-            if (ImGui::CollapsingHeader("Performance", ImGuiTreeNodeFlags_DefaultOpen))
-            {
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-                ImGui::Text("CPU Usage: %.1f%%", cpuUsage);
-                ImGui::Text("System CPU Usage: %.1f%%", cpuSysUsage);
-                ImGui::Text("Memory Usage (Available): %.1f%%", static_cast<float>(memory) / maxMemory * 100.0f);
-                ImGui::Text("Memory Usage (In MB): %.1f MB", static_cast<float>(memory) / 1024.0f / 1024.0f);
-                ImGui::Text("System Memory Usage: %.1f MB", static_cast<float>(memUsage) / 1024.0f / 1024.0f);
-            }
+        //    if (ImGui::CollapsingHeader("Performance", ImGuiTreeNodeFlags_DefaultOpen))
+        //    {
+        //        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+        //        ImGui::Text("CPU Usage: %.1f%%", cpuUsage);
+        //        ImGui::Text("System CPU Usage: %.1f%%", cpuSysUsage);
+        //        ImGui::Text("Memory Usage (Available): %.1f%%", static_cast<float>(memory) / maxMemory * 100.0f);
+        //        ImGui::Text("Memory Usage (In MB): %.1f MB", static_cast<float>(memory) / 1024.0f / 1024.0f);
+        //        ImGui::Text("System Memory Usage: %.1f MB", static_cast<float>(memUsage) / 1024.0f / 1024.0f);
+        //    }
 
-            if (ImGui::CollapsingHeader("Display", ImGuiTreeNodeFlags_DefaultOpen))
-            {
-                ImGui::Text("Resolution: (%d, %d)", _windowResolution.Width, _windowResolution.Height);
-                ImGui::Text("Aspect Ratio: (%.1f)", _windowResolution.GetAspectRatio());
-            }
+        //    if (ImGui::CollapsingHeader("Display", ImGuiTreeNodeFlags_DefaultOpen))
+        //    {
+        //        ImGui::Text("Resolution: (%d, %d)", _windowResolution.Width, _windowResolution.Height);
+        //        ImGui::Text("Aspect Ratio: (%.1f)", _windowResolution.GetAspectRatio());
+        //    }
 
-            if (ImGui::CollapsingHeader("Rendering"), ImGuiTreeNodeFlags_DefaultOpen)
-            {
-                auto api = Tbx::App::GetInstance()->GetGraphicsSettings().Api;
-                switch (api)
-                {
-                    case Tbx::GraphicsApi::DirectX12:
-                        ImGui::Text("API: DX12");
-                        break;
-                    case Tbx::GraphicsApi::Vulkan:
-                        ImGui::Text("API: Vulkan");
-                        break;
-                    case Tbx::GraphicsApi::Metal:
-                        ImGui::Text("API: Metal");
-                        break;
-                }
+        //    if (ImGui::CollapsingHeader("Rendering"), ImGuiTreeNodeFlags_DefaultOpen)
+        //    {
+        //        auto api = Tbx::App::GetInstance()->GetGraphicsSettings().Api;
+        //        switch (api)
+        //        {
+        //            case Tbx::GraphicsApi::DirectX12:
+        //                ImGui::Text("API: DX12");
+        //                break;
+        //            case Tbx::GraphicsApi::Vulkan:
+        //                ImGui::Text("API: Vulkan");
+        //                break;
+        //            case Tbx::GraphicsApi::Metal:
+        //                ImGui::Text("API: Metal");
+        //                break;
+        //        }
 
-                int cameraNumber = 0;
-                auto playSpaces = Tbx::World::GetPlayspaces();
-                for (auto playSpace : playSpaces)
-                {
-                    for (auto camera : Tbx::PlayspaceView<Tbx::Camera>(playSpace))
-                    {
-                        auto& camTrans = playSpace->GetBlockOn<Tbx::Transform>(camera);
-                        ImGui::Text("Camera %d Position: %s", cameraNumber, camTrans.Position.ToString().c_str());
-                        ImGui::Text("Camera %d Rotation: %s", cameraNumber, Tbx::Quaternion::ToEuler(camTrans.Rotation).ToString().c_str());
-                        ImGui::Text("Camera %d Scale: %s", cameraNumber, camTrans.Scale.ToString().c_str());
-                        cameraNumber++;
-                    }
-                }
-            }
+        //        int cameraNumber = 0;
+        //        auto playSpaces = Tbx::World::GetPlayspaces();
+        //        for (auto playSpace : playSpaces)
+        //        {
+        //            for (auto camera : Tbx::PlayspaceView<Tbx::Camera>(playSpace))
+        //            {
+        //                auto& camTrans = playSpace->GetBlockOn<Tbx::Transform>(camera);
+        //                ImGui::Text("Camera %d Position: %s", cameraNumber, camTrans.Position.ToString().c_str());
+        //                ImGui::Text("Camera %d Rotation: %s", cameraNumber, Tbx::Quaternion::ToEuler(camTrans.Rotation).ToString().c_str());
+        //                ImGui::Text("Camera %d Scale: %s", cameraNumber, camTrans.Scale.ToString().c_str());
+        //                cameraNumber++;
+        //            }
+        //        }
+        //    }
 
-            if (ImGui::CollapsingHeader("Input", ImGuiTreeNodeFlags_DefaultOpen))
-            {
-                ImGui::Text("Mouse Position: (%.1f, %.1f)", io.MousePos.x, io.MousePos.y);
-                ImGui::Text("Mouse Delta: (%.1f, %.1f)", io.MouseDelta.x, io.MouseDelta.y);
-            }
+        //    if (ImGui::CollapsingHeader("Input", ImGuiTreeNodeFlags_DefaultOpen))
+        //    {
+        //        ImGui::Text("Mouse Position: (%.1f, %.1f)", io.MousePos.x, io.MousePos.y);
+        //        ImGui::Text("Mouse Delta: (%.1f, %.1f)", io.MouseDelta.x, io.MouseDelta.y);
+        //    }
 
-            ImGui::End();
-        }
+        //    ImGui::End();
+        //}
 
-        // Finally, render
-        {
-            ImGui::Render();
-        }
+        //// Finally, render
+        //{
+        //    ImGui::Render();
+        //}
     }
 
     bool ImGuiDebugViewLayer::IsOverlay()
